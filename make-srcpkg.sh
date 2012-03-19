d56a1990d432212d791b28437d937c72d7907e11
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-03-19 11:46:42 +0100

    Minor changes in make-srcpkg.sh script

--- make-srcpkg.sh
+++ make-srcpkg.sh
@@ -4,14 +4,15 @@ print_usage()
 {
 >&2 cat <<EOF
 
-Usage: ${program_name} [-o|--output-dir BUILD_DIR]
+Usage: ${program_name} [-o|--output-dir OUTPUT_DIR]
   [-h|--help] [-v|--version]
 
-Builds a source package for linux-stable project.
+Builds a source package for linux-stable project. The output directory
+can be specified by -o|--output-dir parameter. If the parameter is not
+provided then current directory is used. The script must be executed
+from source top directory.
 
-Note! This tool must be run from the source top directory.
-
-  -o|--output-dir  output directory; default is current directory
+  -o|--output-dir output directory
   -h|--help       show this information
   -v|--version    show version information
 
@@ -30,41 +31,29 @@ EOF
 
 info() 
 {
-  echo "${program_name}: $1" >&2 
+  echo "${program_name}: $1" >&2
 }
 
 error() 
 {
   echo "${program_name}: Error! $1" >&2
-  exit 1;
+  if [ "$2" != "noexit" ]; then
+    exit 1;
+  fi
 }
 
 program_name=`basename "$0"`
-
-# Get version from git describe command. This produces a version 
-# which is based on last tag in the repositioty. If the currently
-# checked out version is not tagged then a suffix is added
-# indicating the number of commits from the most recent tagged commit
-# plus a unique current commit name plus a 'dirty' flag if the working
-# tree has some uncomited changes.
-version=`git describe --dirty || echo "dirty"`
-
-# The tag is exected to be in v0.0.0+icdtcp3-0.0[.0] format. If that is the case
-# then the 'v' letter is omitted, and '+' sign is changed to '-' otherwies
-# the whole tag is taken as the version.
-version=`echo "${version}" | sed -e 's/^v\([^+]*\)+\(.*\)$/\1-\2/'`
-
+version=`git describe --dirty | sed -e 's/^v\([^+]*\)+\(.*\)$/\1-\2/'`
 tag=`git describe --abbrev=0 | sed -e 's/^[^+]*+\(.*\)$/-\1/'`
 
-build_dir=`pwd`
+output_dir=`pwd`
 
 options=`getopt -o o:hv --long output-dir:,help,version -- "$@"`
 eval set -- "$options"
-
 while true ; do
   case "$1" in
-    -o|--output-dir) build_dir=`cd "$2" && pwd`;
-       test $? -eq 0 || error "Invalid build directory specified"; shift 2 ;;
+    -o|--output-dir) output_dir=`cd "$2" && pwd`;
+       test $? -eq 0 || error "Invalid output directory specified"; shift 2 ;;
     -h|--help) print_usage; exit 0 ;;
     -v|--version) print_version; exit 0 ;;
     --) shift; break ;;
@@ -72,7 +61,7 @@ while true ; do
   esac
 done
 
-test "x$1" = "x" || error "Parsing parametes failedi at '$1'"
+test "x$1" = "x" || error "Parsing parameters failed at '$1'"
 
 temp_dir=`mktemp -d`
 test $? -eq 0 || error "Creating temporary directory '${temp_dir}' failed"
@@ -91,10 +80,10 @@ chmod a+x ${export_dir}/scripts/setlocalversion
 
 info "Creating archive linux-${version}.tar.bz2..."
 cd "${temp_dir}"
-test $? -eq 0 || error "Changing directory to '${build_dir}' failed"
-tar -cjf "${build_dir}/linux-${version}.tar.bz2" "linux-${version}"
+test $? -eq 0 || error "Changing directory to '${output_dir}' failed"
+tar -cjf "${output_dir}/linux-${version}.tar.bz2" "linux-${version}"
 test $? -eq 0 || \
-  error "Creating bzipped tar archive '${build_dir}/linux-${version}.tar.bz2' failed"
+  error "Creating bzipped tar archive '${output_dir}/linux-${version}.tar.bz2' failed"
 
 info "Done"
 
